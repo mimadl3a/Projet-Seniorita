d5e046e584062b187120b0d5f00b7b70e8287ef4
mimadl3a <imad.karaoui@gmail.com>
2014-05-25 12:53:53 +0200

    Ajout button maj auth et creation class utilisateur + provider datab + hashage pass

--- src/Projet/AdminBundle/Controller/AccueilController.php
+++ src/Projet/AdminBundle/Controller/AccueilController.php
@@ -4,10 +4,44 @@ namespace Projet\AdminBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Projet\AdminBundle\Entity\Utilisateur;
 class AccueilController extends Controller{
 	
 	public function indexAction(){
+		//$this->us();
 		return $this->render("ProjetAdminBundle:Default:accueil.html.twig");
+	}
+	public function us(){
+		// Les noms d'utilisateurs à créer
+		$noms = array('winzou', 'John', 'Talus');
+		$roles = array('ROLE_ADMIN','ROLE_USER');
+		
+		$manager = $this->getDoctrine()->getManager();
+		
+		foreach ($noms as $i => $nom) {
+			// On crée l'utilisateur
+			$users[$i] = new Utilisateur($roles);
+		
+			// Le nom d'utilisateur et le mot de passe sont identiques
+			$users[$i]->setUsername($nom);
+			
+			
+			// Le sel et les rôles sont vides pour l'instant
+			$users[$i]->setSalt(substr(md5(time()), 0, 10));
+			
+			$factory = $this->get('security.encoder_factory');
+			$encoder = $factory->getEncoder($users[$i]);
+			$password = $encoder->encodePassword($nom, $users[$i]->getSalt());
+			
+			$users[$i]->setPassword($password);
+		
+		
+			// On le persiste
+			$manager->persist($users[$i]);
+		}
+		
+		// On déclenche l'enregistrement
+		$manager->flush();
 	}
 	
 }
---
+++ src/Projet/AdminBundle/Entity/Utilisateur.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Projet\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Utilisateur
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Projet\AdminBundle\Entity\UtilisateurRepository")
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="salt", type="string", length=255)
+     */
+    private $salt;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="roles", type="array")
+     */
+    private $roles;
+
+    public function __construct(array $roles)
+    {
+    	$this->roles = $roles;
+    }
+    public function eraseCredentials()
+    {
+    }
+    
+    
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Utilisateur
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Utilisateur
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return Utilisateur
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set roles
+     *
+     * @param array $roles
+     * @return Utilisateur
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get roles
+     *
+     * @return array 
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+}
